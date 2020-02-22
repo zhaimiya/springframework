c3f221c15054336f0e820f06158eff124386630b
zhaimiya <zhaimiya@gmail.com>
2020-02-22 18:17:47 +0800

    aop,ioc

--- spring-annotation/src/test/java/org/example/AppTest.java
+++ spring-annotation/src/test/java/org/example/AppTest.java
@@ -1,8 +1,12 @@
 package org.example;
 
-import static org.junit.Assert.assertTrue;
-
+import org.example.dao.impl.UserDaoImpl;
+import org.example.ioc.SpringBean;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
@@ -20,8 +24,13 @@ public class AppTest
 
     @Test
     public void test(){
+        ApplicationContext ac = new ClassPathXmlApplicationContext("spring.xml");
+//        System.out.println(ac.getBean("SpringBean").equals(ac.getBean("SpringBean")));
+//        System.out.println("--- ok ---");
+//        System.out.println(ac.getBean(UserDaoImpl.class));
+
         System.out.println("--- ---");
 
-        System.out.println("--- ok ---");
+
     }
 }
